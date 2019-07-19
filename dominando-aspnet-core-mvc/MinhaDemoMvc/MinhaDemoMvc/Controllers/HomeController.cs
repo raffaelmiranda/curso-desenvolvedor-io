417258ea62d578cbded8b7595744ec1e195eec5e
Rafael <rafaelaugustomiranda@hotmail.com>
2019-07-18 23:50:03 -0300

    inicial

---
+++ dominando-aspnet-core-mvc/MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMvc.Models;
+
+namespace MinhaDemoMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacys()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
