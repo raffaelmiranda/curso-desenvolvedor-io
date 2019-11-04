f9ce4084cfe583cf17fd3e8914381c69f721101b
Rafael Augusto Miranda <rafaelaugustomiranda@hotmail.com>
2019-11-04 16:41:13 -0300

    commit

--- dominando-aspnet-core-mvc/MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedoresController.cs
+++ dominando-aspnet-core-mvc/MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedoresController.cs
@@ -106,6 +106,16 @@ namespace DevIO.App.Controllers
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> AtualizarEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+            
+        }
+
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
---
+++ dominando-aspnet-core-mvc/MinhaAppMvcCompleta/src/DevIO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormataDocumento(this RazorPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
