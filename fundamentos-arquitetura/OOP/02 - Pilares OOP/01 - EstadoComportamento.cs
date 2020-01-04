440b5c57918d9682ee8d8cde4c1c1fd888c38929
Rafael Augusto Miranda <rafaelaugustomiranda@hotmail.com>
2020-01-04 12:31:12 -0300

    commit

---
+++ fundamentos-arquitetura/OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Pessoa
+    {
+        //As propriedades indicam estado da classe
+        public string Nome { get; set; } 
+        public DateTime DataNascimento { get; set; }
+
+        public int CalcularIdade() //Comportamento
+        {
+            //Comportamento pode modiicar o estado das propriedades
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
